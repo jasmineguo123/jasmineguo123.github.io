8b3d07f98d8f01f4bb63300a500787e2301c1916
jasmineguo123 <78282147+jasmineguo123@users.noreply.github.com>
2023-07-02 15:14:08 -0400

    ff

--- templates/home/scripts/cufon-load.js
+++
@@ -1,5 +0,0 @@
-document.write('<scr'+'ipt type="text/javascript" src="/templates/home/scripts/fonts/cufon-helveticaneue.font.js"></scr'+'ipt>');
-document.write('<scr'+'ipt type="text/javascript" src="/templates/home/scripts/fonts/cufon-georgia.font.js"></scr'+'ipt>');
-
-Cufon.set('fontFamily', 'Helvetica Neue LT Pro').replace('#menu', { hover: true, hoverables: { a: true, li: true }})('#submenu', { hover: true, hoverables: { a: true, li: true }})('#catmenu', { hover: true, hoverables: { a: true, li: true }})('.paginationBlock', { hover: true, hoverables: { a: true, li: true }})('.paginationBlockX', { hover: true, hoverables: { a: true, li: true }})('#prev_next', { hover: true, hoverables: { a: true, li: true }})('.caption');
-Cufon.set('fontFamily', 'Georgia').replace('h1', { fontWeight: "normal", hover: true, hoverables: { a: true, li: true } })('h2', { fontWeight: "normal", hover: true, hoverables: { a: true, li: true }})('h3', { fontWeight: "normal", hover: true, hoverables: { a: true, li: true } })('.__newsDate', { fontWeight: "normal" });
