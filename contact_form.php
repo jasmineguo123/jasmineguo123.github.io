38eaeb6656a4ee7b500ed226f30d572ce401b6e3
jasmineguo123 <78282147+jasmineguo123@users.noreply.github.com>
2023-07-04 21:14:05 -0400

    rr

---
+++ contact_form.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailto = "zhguo1@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+	
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: http://www.holmdel-horn-antenna.com/contact.html?mailsend");
+ }
